134501f9e8efa446160380a8d5efe06976cca5d5
clsantana <catherine.santana.penagos@gmail.com>
2017-10-29 22:30:55 -0500

    Se implementan los servicios rest para la HU:CON1 - Consultar listado de productos

--- apps/consumidor/urls.py
+++ apps/consumidor/urls.py
@@ -5,5 +5,6 @@ from ..consumidor import views
 urlpatterns = [
     url(r'catalogo_compras/$', views.catalogo_compras, name='catalogo_compras'),
     url(r'^agregar_producto/$', views.agregar_producto, name='agregar_producto'),
-
-    ]
+    url(r'^listar_productos_catalogo/$', views.listar_productos_catalogo_view, name='listar_productos_catalogo'),
+    url(r'^seleccionarProductos/$', views.select_productos, name="seleccionarProductos"),
+]
--- apps/consumidor/views.py
+++ apps/consumidor/views.py
@@ -2,10 +2,13 @@
 from __future__ import unicode_literals
 
 import json
+
+from django.core.serializers.json import DjangoJSONEncoder
 from django.views.decorators.csrf import csrf_exempt
 
 from ..consumidor.models import ProductoCatalogo
-from django.http import JsonResponse
+from ..administrador.models import CatalogoProductos, Producto
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -25,3 +28,30 @@ def agregar_producto(request):
                                  id_producto_catalogo=producto)
         prodAdd.save()
     return JsonResponse({"mensaje": "ok"})
+
+@csrf_exempt
+def listar_productos_catalogo_view(request):
+    if request.method == 'GET':
+        listaCatalogoProductos = CatalogoProductos.objects.filter(activo=1)
+        listaProductosCatalogo = ProductoCatalogo.objects.filter(id_catalogo = listaCatalogoProductos[0].id)
+        data_productos_catalogo = [{'id': productoCatalogo.id,
+                                    'producto': productoCatalogo.id_producto.nombre,
+                                    'foto': str(productoCatalogo.id_producto.foto),
+                                    'unidad':productoCatalogo.id_producto.id_tipo_unidad.abreviatura,
+                                    'precio': productoCatalogo.precio_definido}for productoCatalogo in listaProductosCatalogo]
+
+    data_convert = json.dumps(data_productos_catalogo,cls=DjangoJSONEncoder)
+    return HttpResponse(data_convert)
+
+@csrf_exempt
+def select_productos(request):
+    if request.method == "GET":
+        productos = Producto.objects.filter(activo=True)
+        lista_productos = [{'id': producto.id,
+                            'nombre': producto.nombre,
+                            'descripcion': producto.descripcion,
+                            'imagen': str(producto.foto),
+                            'activo': producto.activo} for producto in productos]
+
+        data_convert = json.dumps(lista_productos)
+        return HttpResponse(data_convert)
