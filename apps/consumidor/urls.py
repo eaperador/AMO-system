8b7abebe1a3bda91248e65fe0ce66b75c75d31c4
bamontanez <ba.montanez@uniandes.edu.co>
2017-10-31 01:35:38 -0500

    se arregla el carrito de compra para que use el modal, y ajustes por modelo

--- apps/consumidor/urls.py
+++ apps/consumidor/urls.py
@@ -4,6 +4,6 @@ from ..consumidor import views
 
 urlpatterns = [
     url(r'catalogo_compras/$', views.catalogo_compras, name='catalogo_compras'),
-    url(r'^agregar_producto/$', views.agregar_producto, name='agregar_producto'),
+    url(r'catalogo_compras/agregar_producto/$', views.agregar_producto, name='agregar_producto'),
 
     ]
--- apps/consumidor/views.py
+++ apps/consumidor/views.py
@@ -4,24 +4,27 @@ from __future__ import unicode_literals
 import json
 from django.views.decorators.csrf import csrf_exempt
 
-from ..consumidor.models import ProductoCatalogo
-from django.http import JsonResponse
+from ..consumidor.models import ItemCompra, Compra
+from ..administrador.models import CatalogoProductos, Producto, ProductoCatalogo
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
 
 @csrf_exempt
 def catalogo_compras(request):
-    lista_productos = ProductoCatalogo.objects.all()
-    return render(request, "catalogoCompras.html", {'productos':lista_productos})
+    listaCatalogoProductos = CatalogoProductos.objects.filter(activo=1)
+    listaProductosCatalogo = ProductoCatalogo.objects.filter(id_catalogo=listaCatalogoProductos[0].id)
+    return render(request, "catalogoCompras.html", {'productos':listaProductosCatalogo})
 
 @csrf_exempt
 def agregar_producto(request):
+    print request.method
     if request.method == 'POST':
         jsonOferta = json.loads(request.body)
         productoId = jsonOferta['productoId']
+        cantidad = jsonOferta['cantidad']
         producto = ProductoCatalogo.objects.get(id=productoId)
-        prodAdd = ProductoCatalogo(cantidad='1',
+        prodAdd = ItemCompra(cantidad=cantidad,
                                  id_producto_catalogo=producto)
         prodAdd.save()
     return JsonResponse({"mensaje": "ok"})
