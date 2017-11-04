6fd7e6aa3c6d4b389fcffb3dbb11d549a0fa7046
clsantana <catherine.santana.penagos@gmail.com>
2017-11-04 11:46:16 -0500

    ROJO: Test que verifica que se están cargando todos los productos de las ofertas en el combo

--- apps/productor/tests.py
+++ apps/productor/tests.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 class ProductorTestCase(TestCase):
 
     def setUp(self):
-        #self.browser = webdriver.Chrome("C:\\Users\\Oscar Amaya\\Documents\\tmp\\delete\\chromedriver33.exe")
+        #self.browser = webdriver.Chrome("C:\\Users\\CATHERIN\\Documents\\chromedriver.exe")
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(2)
 
@@ -24,3 +24,13 @@ class ProductorTestCase(TestCase):
         opCount = len(listaProductos.find_elements_by_tag_name("option"))
         self.assertEquals(1, opCount)
         self.assertEquals('Todos...', listaProductos.find_elements_by_tag_name("option")[0].text)
+
+    def test_filter_a_lot_of_products(self):
+        self.browser.get('http://localhost:8000/productor/ver_ofertas')
+        self.browser.implicitly_wait(10)
+
+        span = self.browser.find_element(By.XPATH, '//label[text()="Producto:"]')
+        self.assertIn('Producto:', span.text)
+
+        listaProductos = self.browser.find_element_by_id('listaProductos')
+        self.assertTrue(len(listaProductos.find_elements_by_tag_name("option")) > 1)
