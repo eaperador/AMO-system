ea707231d2d006735c7261f5df4e1c461e8d2a58
Oscar amaya <oamaya@lacadenaretail.com>
2017-11-05 02:43:34 -0500

    ROJO: Test de filtrado por producto

--- apps/productor/tests.py
+++ apps/productor/tests.py
@@ -7,8 +7,8 @@ from selenium.webdriver.common.by import By
 class ProductorTestCase(TestCase):
 
     def setUp(self):
-        #self.browser = webdriver.Chrome("")
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Chrome("C:\\Users\\Oscar Amaya\\Documents\\tmp\\delete\\chromedriver31.exe")
+        #self.browser = webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
 
@@ -68,3 +68,36 @@ class ProductorTestCase(TestCase):
 
         listaProductos = self.browser.find_element_by_id('listaProductos')
         self.assertTrue(len(listaProductos.find_elements_by_tag_name("option")) > 1)
+
+    def test_filter_a_lot_of_products(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('iniciar_sesion')
+        link.click()
+        sleep(1)
+
+        nombreUsuario = self.browser.find_element_by_id('inputUsername')
+        nombreUsuario.send_keys('prod1')
+        sleep(1)
+
+        clave = self.browser.find_element_by_id('inputPassword')
+        clave.send_keys('usuarioprod1')
+        sleep(1)
+
+        botonLogin = self.browser.find_element_by_id('btn_iniciarSesion')
+        botonLogin.click()
+        sleep(2)
+
+        continue_link = self.browser.find_element_by_id('id_consulatarOfertas')
+        continue_link.click()
+        sleep(1)
+
+        span = self.browser.find_element(By.XPATH, '//label[text()="Producto:"]')
+        self.assertIn('Producto:', span.text)
+
+        self.browser.find_element_by_xpath("//select[@id='listaProductos']/option[text()='Naranja']").click()
+        sleep(5)
+
+        offer_list = self.browser.find_element_by_id('listaOfertas')
+        otherProds = 0
+        for offer in offer_list.find_elements_by_tag_name('tr'):
+            self.assertEquals(offer.find_elements_by_tag_name('td')[1].text, 'papa')
