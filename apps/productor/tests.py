a6d360f70e0b7bd998bb9fe92a53146de2971cc0
Oscar amaya <oamaya@lacadenaretail.com>
2017-11-04 20:24:34 -0500

    prueba de consulta de usuario

--- apps/productor/tests.py
+++ apps/productor/tests.py
@@ -39,7 +39,6 @@ class ProductorTestCase(TestCase):
         self.catalogo.save()
         self.oferta = Oferta(precio=5000,cantidad=1000,cantidad_disponible=100,fecha=datetime.datetime.today(),id_catalogo_oferta=self.catalogo,id_producto=self.producto1,id_productor=self.usuario,id_estado_oferta=self.estadoOferta)
         self.oferta.save()
-        print (User.objects.all())
     def tearDown(self):
         self.oferta.delete()
         self.catalogo.delete()
@@ -66,6 +65,7 @@ class ProductorTestCase(TestCase):
         nombreUsuario.send_keys('prodTest')
         sleep(1)
 
+        print (User.objects.get(id=1).password)
         clave = self.browser.find_element_by_id('inputPassword')
         clave.send_keys('userprodtest')
         sleep(1)
