4b4a344c3ef844f3cf3fe225b86b0543593ccf85
bamontanez <ba.montanez@uniandes.edu.co>
2017-11-05 04:32:11 -0500

    VERDE: Ajuste para test

--- apps/productor/tests.py
+++ apps/productor/tests.py
@@ -25,8 +25,8 @@ def login(self):
 
 class ProductorTestCase(TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome("C:\\chromedriver\\chromedriver.exe")
-        # self.browser = webdriver.Chrome()
+        # self.browser = webdriver.Chrome("C:\\chromedriver\\chromedriver.exe")
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
@@ -78,8 +78,8 @@ class ProductorTestCase(TestCase):
 
 class ProductorEditOfertaTestCase(TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome("C:\\chromedriver\\chromedriver.exe")
-        # self.browser = webdriver.Chrome()
+        # self.browser = webdriver.Chrome("C:\\chromedriver\\chromedriver.exe")
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
