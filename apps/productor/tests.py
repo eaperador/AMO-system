ffcaa6e0aca0ddc72aee30792095b68f34492e61
Oscar amaya <oamaya@lacadenaretail.com>
2017-11-05 03:09:46 -0500

    AZUL:Test de filtrado por producto - refactor

--- apps/productor/tests.py
+++ apps/productor/tests.py
@@ -4,32 +4,36 @@ from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
+def login(self):
+    link = self.browser.find_element_by_id('iniciar_sesion')
+    link.click()
+    sleep(1)
+
+    nombreUsuario = self.browser.find_element_by_id('inputUsername')
+    nombreUsuario.send_keys('prod1')
+    sleep(1)
+
+    clave = self.browser.find_element_by_id('inputPassword')
+    clave.send_keys('usuarioprod1')
+    sleep(1)
+
+    botonLogin = self.browser.find_element_by_id('btn_iniciarSesion')
+    botonLogin.click()
+    sleep(2)
+
+
 class ProductorTestCase(TestCase):
 
     def setUp(self):
-        #self.browser = webdriver.Chrome("C:\\Users\\Oscar Amaya\\Documents\\tmp\\delete\\chromedriver31.exe")
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Chrome("C:\\Users\\Oscar Amaya\\Documents\\tmp\\delete\\chromedriver31.exe")
+        #self.browser = webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
 
     def test_filter(self):
         self.browser.get('http://localhost:8000')
-        link = self.browser.find_element_by_id('iniciar_sesion')
-        link.click()
-        sleep(1)
-
-        nombreUsuario = self.browser.find_element_by_id('inputUsername')
-        nombreUsuario.send_keys('prod1')
-        sleep(1)
-
-        clave = self.browser.find_element_by_id('inputPassword')
-        clave.send_keys('usuarioprod1')
-        sleep(1)
-
-        botonLogin = self.browser.find_element_by_id('btn_iniciarSesion')
-        botonLogin.click()
-        sleep(1)
-
+        login(self)
         continue_link = self.browser.find_element_by_id('id_consulatarOfertas')
         continue_link.click()
         sleep(1)
@@ -43,22 +47,7 @@ class ProductorTestCase(TestCase):
 
     def test_filter_a_lot_of_products(self):
         self.browser.get('http://localhost:8000')
-        link = self.browser.find_element_by_id('iniciar_sesion')
-        link.click()
-        sleep(1)
-
-        nombreUsuario = self.browser.find_element_by_id('inputUsername')
-        nombreUsuario.send_keys('prod1')
-        sleep(1)
-
-        clave = self.browser.find_element_by_id('inputPassword')
-        clave.send_keys('usuarioprod1')
-        sleep(1)
-
-        botonLogin = self.browser.find_element_by_id('btn_iniciarSesion')
-        botonLogin.click()
-        sleep(2)
-
+        login(self)
         continue_link = self.browser.find_element_by_id('id_consulatarOfertas')
         continue_link.click()
         sleep(1)
@@ -71,22 +60,7 @@ class ProductorTestCase(TestCase):
 
     def test_filter_a_lot_of_products(self):
         self.browser.get('http://localhost:8000')
-        link = self.browser.find_element_by_id('iniciar_sesion')
-        link.click()
-        sleep(1)
-
-        nombreUsuario = self.browser.find_element_by_id('inputUsername')
-        nombreUsuario.send_keys('prod1')
-        sleep(1)
-
-        clave = self.browser.find_element_by_id('inputPassword')
-        clave.send_keys('usuarioprod1')
-        sleep(1)
-
-        botonLogin = self.browser.find_element_by_id('btn_iniciarSesion')
-        botonLogin.click()
-        sleep(2)
-
+        login(self)
         continue_link = self.browser.find_element_by_id('id_consulatarOfertas')
         continue_link.click()
         sleep(1)
