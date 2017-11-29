001ea64e2cca961cc3c5dcc0d1fa062b1fae8ade
Oscar Amaya <og.amaya@uniandes.edu.co>
2017-11-28 23:02:32 -0500

    ROJO: Test de opción de menú disponible

--- apps/administrador/tests.py
+++ apps/administrador/tests.py
@@ -1,12 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from time import sleep
+
 from django.test import TestCase
 
 # Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def login(self):
+    link = self.browser.find_element_by_id('iniciar_sesion')
+    link.click()
+    sleep(1)
+
+    nombreUsuario = self.browser.find_element_by_id('inputUsername')
+    nombreUsuario.send_keys('administrador')
+    sleep(1)
+
+    clave = self.browser.find_element_by_id('inputPassword')
+    clave.send_keys('usuarioA')
+    sleep(1)
+
+    botonLogin = self.browser.find_element_by_id('btn_iniciarSesion')
+    botonLogin.click()
+    sleep(2)
+
 class AdministradorTestCase(TestCase):
-    # Para verificar la integracion con CodeShip
-    def test_echo_for_CI(self):
-        """Animals that can speak are correctly identified"""
-        print ("Ejecuta el test de Administrador")
-        self.assertEqual('Modulo administrador', 'Modulo administrador')
+
+    def setUp(self):
+        self.browser = webdriver.Chrome("C:\\To_D\\chromedriver.exe")
+        #self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_menu_available(self):
+        self.browser.get('http://localhost:8000')
+
+        sleep(2)
+        login(self)
+        sleep(5)
+        a = self.browser.find_element(By.XPATH, '//a[text()="Cerrar semana"]')
+        self.assertIn('CERRAR SEMANA', a.text)
